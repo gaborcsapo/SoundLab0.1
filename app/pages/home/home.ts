bc08cf260663b37817fae4c66fc0396497b91e57
Gabor <csgabor14@gmail.com>
2016-08-07 15:38:56 +0200

    cool and working filters

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -15,32 +15,55 @@ export class HomePage {
   currentFilter:any;
   currentProps:any;
   tuna = new Tuna(this.context);
-  filters = {'chorus': {'rate':{'value': 0, 'min':0.01, 'max':8},
-                        'feedback':{'value': 0, 'min':0, 'max':1},
-                        'delay':{'value': 0, 'min':0, 'max':1},
-                        'bypass':{'value': 0, 'min':0, 'max':1}}
+  filters = {'Chorus': {'displayed': {'rate':{'value': 0, 'min':0.01, 'max':8, 'log':true},
+                                      'feedback':{'value': 0, 'min':0, 'max':1, 'log':false},
+                                      'delay':{'value': 0, 'min':0, 'max':1, 'log':true}},
+                        'type': 'none'},
+              'Lowpass':{'displayed': {'frequency':{'value': 20, 'min':20, 'max':22050, 'log':true},
+                                      'Q':{'value': 0.001, 'min':0.001, 'max':100, 'log':true},
+                                      'gain':{'value': -40, 'min':-40, 'max':40, 'log':true}},
+                         'type':'lowpass'},
+              'Highpass':{'displayed': {'frequency':{'value': 20, 'min':20, 'max':22050, 'log':true},
+                                        'Q':{'value': 0.001, 'min':0.001, 'max':100, 'log':true},
+                                        'gain':{'value': -40, 'min':-40, 'max':40, 'log':true}},
+                         'type':'highpass'},
+              'Bitcrusher': {'displayed': {'bits':{'value': 1, 'min':1, 'max':16, 'log':true},
+                                          'normfreq':{'value': 0, 'min':0, 'max':1, 'log':true},
+                                          'bufferSize':{'value': 256, 'min':256, 'max':16384, 'log':true}},
+                            'type': 'none'}
             }//the filter data should be stored in a service and should be "get" through that
-  
-  chooseFilter(event){
-    // first i have to diconnect the previous one
-    this.currentFilter = this.filters['chorus'];
-    this.currentProps = Object.keys(this.currentFilter);//read this !!!https://webcake.co/object-properties-in-angular-2s-ngfor/ implement as a pipe and only input currentFilter in the end
-    this.filter = new this.tuna['Chorus']({
-        rate: 1.5,
-        feedback: 0.2,
-        delay: 0.0045,
-        bypass: 0
-    });
+
+  chooseFilter(event){   
+    if (this.filter != undefined)
+      this.filter.disconnect();
+ 
+    this.currentFilter = this.filters[event];
+
+    if (this.filters[event]['type'] != 'none'){
+      this.filter = new this.tuna['Filter']();
+      this.filter['filterTpye'] = this.currentFilter['type'];
+    } else {
+      this.filter = new this.tuna[event]();
+    }
+    
+    this.currentProps = Object.keys(this.currentFilter['displayed']);//read this !!!https://webcake.co/object-properties-in-angular-2s-ngfor/ implement as a pipe and only input currentFilter in the end
+    
+    for (var item of this.currentProps) {
+      this.filter[item] = this.currentFilter['displayed'][item]['min']
+    }
+    
     this.source.connect(this.filter);
     this.filter.connect(this.context.destination);
   }
 
   update(item, amount){
-    var change = amount/(100/(this.currentFilter[item]['max']-this.currentFilter[item]['min']));
-    console.log(change);
-    this.currentFilter[item]['value'] = change;
-    this.filter[item] = change;
-    
+    var toChange = this.currentFilter['displayed'][item]
+    var change = toChange['min']+(amount/100)*(toChange['max']-toChange['min']);
+    if (toChange['log'] == true) {
+      change = toChange['min'] + (Math.pow(2, amount*(Math.log2(toChange['max']-toChange['min']+1)/100))-1);
+    }
+    this.currentFilter['displayed'][item]['value'] = change;
+    this.filter[item] = change;  
   }
 
   ngOnInit(){
